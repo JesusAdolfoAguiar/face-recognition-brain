72eec9bc80b9edf7235496c9f60059bff8c94bb3
Jesus Aguiar <j.adolfo.3@gmail.com>
2023-08-28 11:34:45 -0400

    gh

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -33,6 +33,7 @@ class Register extends React.Component {
       })
     })
       .then(response => response.json())
+      .then(resposne => console.log(response))
       .then(user => {
         if (user.id) {
           this.props.loadUser(user)
