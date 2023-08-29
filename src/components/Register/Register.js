9a4a068bb49f568e9493c5fceef68f96c630941e
Jesus Aguiar <j.adolfo.3@gmail.com>
2023-08-29 10:32:23 -0400

    go

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://face-recognition-brain-uo9p.onrender.com/register', {
+    fetch('https://face-recognition-brain-uo9p.onrender.com', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
